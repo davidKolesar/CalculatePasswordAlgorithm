6a88c584f4e5399e8b6b7a97e88b2877f5cc84a4
unknown <djkolesa@gmail.com>
2022-08-14 13:43:57 -0700

    creating output

--- lib/src/main/java/com/mycompany/passwordLogic/PasswordLogic.java
+++ lib/src/main/java/com/mycompany/passwordLogic/PasswordLogic.java
@@ -1,24 +1,22 @@
 package com.mycompany.passwordLogic;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class PasswordLogic {
 
 	/**
-	 * Takes in String of potential passwords and enforces the following rules:
-	 * it has to contain only alphanumerical characters (a−z, A−Z, 0−9); 
-	 * there should be an even number of letters; 
-	 * there should be an odd number of digits.
+	 * Takes in String of potential passwords and enforces the following rules: it
+	 * has to contain only alphanumeric characters (a−z, A−Z, 0−9); there should be
+	 * an even number of letters; there should be an odd number of digits.
 	 * 
 	 * @param potentialPasswords
 	 */
 	public void passwordCalculator(String potentialPasswords) {
 		String[] splitPasswords = potentialPasswords.split("\\s+");
 		Stream<String> evaluatePotentialPasswords = Arrays.stream(splitPasswords);
-		List<String> validPasswords = evaluatePotentialPasswords
+		String longestValidPassword = evaluatePotentialPasswords
 				// filter out passwords that are not alphanumeric
 				.filter(x -> x.matches("^[a-zA-Z0-9]*$"))
 				// filter out passwords with even number of digits
@@ -26,11 +24,11 @@ public class PasswordLogic {
 				// Filter out password with odd string lengths
 				.filter(x -> x.chars().filter(Character::isLetter).count() % 2 == 0)
 				// Collect the remaining results
-				.collect(Collectors.toList());
+				.max(Comparator.comparingInt(String::length)).get();
 
-		System.out.println(("Valid passwords:"));
-		for (int i = 0; i < validPasswords.size(); i++) {
-			System.out.println(validPasswords.get(i));
+		if (longestValidPassword == null || longestValidPassword.trim().isEmpty()) {
+			longestValidPassword = "no acceptable passwords";
 		}
+		System.out.println(("Longest Valid password: " + longestValidPassword));
 	}
 }
