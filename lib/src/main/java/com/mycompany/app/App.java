96a4107a2fbf071458458d2550ffde41b2146f1b
unknown <djkolesa@gmail.com>
2022-08-14 13:20:45 -0700

    working on solution

--- lib/src/main/java/com/mycompany/app/App.java
+++ lib/src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,7 @@ import com.mycompany.passwordLogic.PasswordLogic;
 public class App {
 
 	public static void main(String[] args) {
-		int[] x = {-2, 0, 1, 4};
 		PasswordLogic passwordLogic = new PasswordLogic();
-		passwordLogic.passwordCalculator("Hsdf asdf safd s");
+		passwordLogic.passwordCalculator("test 5 a0A pass007 ?xy1");
 		}	
 }
--- lib/src/main/java/com/mycompany/passwordLogic/PasswordLogic.java
+++ lib/src/main/java/com/mycompany/passwordLogic/PasswordLogic.java
@@ -1,8 +1,35 @@
 package com.mycompany.passwordLogic;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class PasswordLogic {
 
-	public int passwordCalculator(String S) {
-		return -1;
+	public int passwordCalculator(String potentialPasswords) {
+		String[] splitPasswords = potentialPasswords.split("\\s+");
+		Stream<String> evaluatePotentialPasswords = Arrays.stream(splitPasswords);
+
+		List<String> validPasswords = evaluatePotentialPasswords
+				// filter out passwords that are not alphanumeric
+				.filter(x -> x.chars().filter(Character::isAlphabetic))
+				// filter out passwords with even number of digits
+				.filter(x -> x.chars().filter(Character::isDigit).count() % 2 != 0)
+				// Filter out password with odd string lengths
+				.filter(x -> x.chars().filter(Character::isLetter).count() % 2 == 0)
+				// Collect the remaining results
+				.collect(Collectors.toList());
+
+		System.out.println(("Valid passwords:"));
+		for(int i=0; i < validPasswords.size(); i++) {
+			System.out.println(validPasswords.get(i));
+		}
+		
+		if (validPasswords.size() == 0) {
+			return -1;
+		}
+
+		return validPasswords.size();
 	}
 }
