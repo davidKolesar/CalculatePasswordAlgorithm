bc3ad057aa736f81ab392cbb7afb3382a86ffcfc
unknown <djkolesa@gmail.com>
2022-08-14 13:21:13 -0700

    Solved it

--- lib/src/main/java/com/mycompany/passwordLogic/PasswordLogic.java
+++ lib/src/main/java/com/mycompany/passwordLogic/PasswordLogic.java
@@ -7,13 +7,12 @@ import java.util.stream.Stream;
 
 public class PasswordLogic {
 
-	public int passwordCalculator(String potentialPasswords) {
+	public int passwordCalculator(String potentialPasswords) {		
 		String[] splitPasswords = potentialPasswords.split("\\s+");
 		Stream<String> evaluatePotentialPasswords = Arrays.stream(splitPasswords);
-
 		List<String> validPasswords = evaluatePotentialPasswords
 				// filter out passwords that are not alphanumeric
-				.filter(x -> x.chars().filter(Character::isAlphabetic))
+				.filter(x -> x.matches("^[a-zA-Z0-9]*$"))
 				// filter out passwords with even number of digits
 				.filter(x -> x.chars().filter(Character::isDigit).count() % 2 != 0)
 				// Filter out password with odd string lengths
