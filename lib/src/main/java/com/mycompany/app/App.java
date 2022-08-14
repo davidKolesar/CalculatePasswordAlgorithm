c20a04f9e5b282847bfac8440d9d2a9df01478a4
unknown <djkolesa@gmail.com>
2022-08-14 13:45:10 -0700

    ready to demo

--- lib/src/main/java/com/mycompany/app/App.java
+++ lib/src/main/java/com/mycompany/app/App.java
@@ -7,5 +7,5 @@ public class App {
 	public static void main(String[] args) {
 		PasswordLogic passwordLogic = new PasswordLogic();
 		passwordLogic.passwordCalculator("test 5 a0A pass007 ?xy1");
-		}	
+	}
 }
